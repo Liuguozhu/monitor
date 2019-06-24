c4b21dbeec7bb4a1972ec33af3dcdeefd8d7fe7c
Liugz <liuguozhu30@163.com>
2019-06-24 15:14:46 +0800

    增加系统配置页

--- src/main/java/com/coder/monitor/controller/admin/BaseController.java
+++ src/main/java/com/coder/monitor/controller/admin/BaseController.java
@@ -1,5 +1,6 @@
 package com.coder.monitor.controller.admin;
 
+import com.coder.monitor.common.Result;
 import com.coder.monitor.service.SystemConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -15,6 +16,25 @@ public class BaseController {
   // 只针对前台页面的模板路径渲染，后台不变
   protected String render(String path) {
     return String.format("theme/%s/%s", systemConfigService.selectAllConfig().get("theme").toString(), path);
+  }
+
+  protected Result success() {
+    return success(null);
+  }
+
+  protected Result success(Object detail) {
+    Result result = new Result();
+    result.setCode(200);
+    result.setDescription("SUCCESS");
+    result.setDetail(detail);
+    return result;
+  }
+
+  protected Result error(String description) {
+    Result result = new Result();
+    result.setCode(201);
+    result.setDescription(description);
+    return result;
   }
 
 }
--- src/main/resources/db/migration/V1__initialize.sql
+++ src/main/resources/db/migration/V1__initialize.sql
@@ -185,67 +185,62 @@ VALUES
 /*!40000 ALTER TABLE `role_permission` ENABLE KEYS */;
 UNLOCK TABLES;
 
-
-# Dump of table system_config
-# ------------------------------------------------------------
-
 DROP TABLE IF EXISTS `system_config`;
-
-CREATE TABLE `system_config` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `key` varchar(255) DEFAULT NULL,
-  `value` varchar(255) DEFAULT NULL,
-  `description` varchar(1000) NOT NULL,
-  `pid` int(11) NOT NULL DEFAULT '0',
-  `type` varchar(255) DEFAULT '',
-  `option` varchar(255) DEFAULT NULL,
-  `reboot` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `system_config`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `key` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `value` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '',
+  `description` varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `pid` int(11) NOT NULL DEFAULT 0,
+  `type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `option` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `reboot` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 67 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 LOCK TABLES `system_config` WRITE;
 /*!40000 ALTER TABLE `system_config` DISABLE KEYS */;
-
-INSERT INTO `system_config` (`id`, `key`, `value`, `description`, `pid`, `type`, `option`, `reboot`)
-VALUES
-	(23, NULL, NULL, '基础配置', 0, NULL, NULL, 0),
-	(24, NULL, NULL, '邮箱配置', 0, NULL, NULL, 0),
-	(25, NULL, NULL, '上传配置', 0, NULL, NULL, 0),
-	(27, NULL, NULL, 'Redis配置', 0, NULL, NULL, 0),
-	(35, NULL, NULL, 'Elasticsearch配置', 0, NULL, NULL, 0),
-	(40, NULL, NULL, 'Github登录配置，<a href=\"https://github.com/settings/developers\" target=\"_blank\">申请地址</a>', 0, '', NULL, 0),
-	(45, NULL, NULL, 'WebSocket，开启后可不用刷新页面接收页面消息', 0, '', NULL, 0),
-	(1, 'admin_remember_me_max_age', '30', '登录后台记住我功能记住时间，单位：天', 23, 'number', NULL, 1),
-	(2, 'base_url', 'http://localhost:8080', '网站部署后访问的域名，注意这个后面没有 \"/\"', 23, 'url', NULL, 0),
-	(4, 'cookie_domain', 'localhost', '存cookie时用到的域名，要与网站部署后访问的域名一致', 23, 'text', NULL, 0),
-	(5, 'cookie_max_age', '604800', 'cookie有效期，单位秒，默认1周', 23, 'number', NULL, 0),
-	(6, 'cookie_name', 'user_token', '存cookie时用到的名称', 23, 'text', NULL, 0),
-	(11, 'intro', '<h5>基于Java写的服务器监控</h5><p>在这里，您可以配置需要监控的服务，以及用于通知的方式，接收服务异常通知的人员！</p>', '项目介绍', 23, 'text', NULL, 0),
-	(15, 'name', 'monitor控制台', '站点名称', 23, 'text', NULL, 0),
-	(16, 'page_size', '20', '分页每页条数', 23, 'number', NULL, 0),
-	(39, 'search', '0', '是否开启搜索功能（如果开启，需要额外启动一个ES服务，并填好ES相关的配置）', 35, 'radio', NULL, 0),
-	(12, 'mail_host', 'smtp.qq.com', '邮箱的smtp服务器地址', 24, 'text', NULL, 0),
-	(13, 'mail_password', '', '发送邮件的邮箱密码', 24, 'password', NULL, 0),
-	(14, 'mail_username', 'xxoo@qq.com', '发送邮件的邮箱地址', 24, 'email', NULL, 0),
-	(18, 'static_url', 'http://localhost:8080/static/upload/', '静态文件访问地址，主要用于上传图片的访问，注意最后有个\"/\"', 25, 'url', NULL, 0),
-	(20, 'upload_avatar_size_limit', '2', '上传头像文件大小，单位MB，默认2MB', 25, 'number', NULL, 0),
-	(21, 'upload_path', '/opt/monitor/static/upload/', '上传文件的路径，注意最后有个\"/\"', 25, 'text', NULL, 0),
-	(29, 'redis_host', '', 'redis服务host地址', 27, 'text', NULL, 0),
-	(30, 'redis_port', '', 'redis服务端口（默认: 6379）', 27, 'number', NULL, 0),
-	(31, 'redis_password', '', 'redis服务密码', 27, 'password', NULL, 0),
-	(32, 'redis_timeout', '2000', '网站连接redis服务超时时间，单位毫秒', 27, 'number', NULL, 0),
-	(33, 'redis_database', '0', '网站连接redis服务的哪个数据库，默认0号数据库，取值范围0-15', 27, 'number', NULL, 0),
-	(34, 'redis_ssl', '0', 'redis服务是否开启认证连接', 27, 'radio', NULL, 0),
-	(36, 'elasticsearch_host', '', 'elasticsearch服务的地址', 35, 'text', NULL, 0),
-	(37, 'elasticsearch_port', '', 'elasticsearch服务的http端口', 35, 'number', NULL, 0),
-	(38, 'elasticsearch_index', '', '索引的名字', 35, 'text', NULL, 0),
-	(41, 'oauth_github_client_id', '', 'Github登录配置项ClientId', 40, 'text', NULL, 0),
-	(42, 'oauth_github_client_secret', '', 'Github登录配置项ClientSecret', 40, 'text', NULL, 0),
-	(43, 'oauth_github_callback_url', '', 'Github登录配置项回调地址', 40, 'url', NULL, 0),
-	(17, 'websocket', '0', '是否开启websocket功能', 45, 'radio', NULL, 1),
-	(46, 'websocket_host', '', 'websocket服务的主机名，这个跟cookie的域名设置成一样的就可以了', 45, 'text', NULL, 1),
-	(47, 'websocket_port', '', 'websocket服务的端口，不能跟论坛服务端口一样，其它随便设置', 45, 'number', NULL, 1),
-	(48, 'theme', 'default', '系统主题', 23, 'select', 'default', 0);
+INSERT INTO `system_config` VALUES (1, 'admin_remember_me_max_age', '30', '登录后台记住我功能记住时间，单位：天', 23, 'number', NULL, 1);
+INSERT INTO `system_config` VALUES (2, 'base_url', 'http://localhost:8080', '网站部署后访问的域名，注意这个后面没有 \"/\"', 23, 'url', NULL, 0);
+INSERT INTO `system_config` VALUES (4, 'cookie_domain', 'localhost', '存cookie时用到的域名，要与网站部署后访问的域名一致', 23, 'text', NULL, 0);
+INSERT INTO `system_config` VALUES (5, 'cookie_max_age', '604800', 'cookie有效期，单位秒，默认1周', 23, 'number', NULL, 0);
+INSERT INTO `system_config` VALUES (6, 'cookie_name', 'user_token', '存cookie时用到的名称', 23, 'text', NULL, 0);
+INSERT INTO `system_config` VALUES (11, 'intro', '<h5>基于Java写的服务器监控</h5><p>在这里，您可以配置需要监控的服务，以及用于通知的方式，接收服务异常通知的人员！</p>', '项目介绍', 23, 'text', NULL, 0);
+INSERT INTO `system_config` VALUES (12, 'mail_host', 'smtp.qq.com', '邮箱的smtp服务器地址', 24, 'text', NULL, 0);
+INSERT INTO `system_config` VALUES (13, 'mail_password', '', '发送邮件的邮箱密码', 24, 'password', NULL, 0);
+INSERT INTO `system_config` VALUES (14, 'mail_username', 'xxoo@qq.com', '发送邮件的邮箱地址', 24, 'email', NULL, 0);
+INSERT INTO `system_config` VALUES (15, 'name', 'monitor控制台', '站点名称', 23, 'text', NULL, 0);
+INSERT INTO `system_config` VALUES (16, 'page_size', '20', '分页每页条数', 23, 'number', NULL, 0);
+INSERT INTO `system_config` VALUES (18, 'static_url', 'http://localhost:8080/static/upload/', '静态文件访问地址，主要用于上传图片的访问，注意最后有个\"/\"', 25, 'url', NULL, 0);
+INSERT INTO `system_config` VALUES (20, 'upload_avatar_size_limit', '2', '上传头像文件大小，单位MB，默认2MB', 25, 'number', NULL, 0);
+INSERT INTO `system_config` VALUES (21, 'upload_path', '/opt/monitor/static/upload/', '上传文件的路径，注意最后有个\"/\"', 25, 'text', NULL, 0);
+INSERT INTO `system_config` VALUES (23, NULL, NULL, '基础配置', 0, NULL, NULL, 0);
+INSERT INTO `system_config` VALUES (24, NULL, NULL, '邮箱配置', 0, NULL, NULL, 0);
+INSERT INTO `system_config` VALUES (25, NULL, NULL, '上传配置', 0, NULL, NULL, 0);
+INSERT INTO `system_config` VALUES (27, NULL, NULL, 'Redis配置', 0, NULL, NULL, 0);
+INSERT INTO `system_config` VALUES (29, 'redis_host', '', 'redis服务host地址', 27, 'text', NULL, 0);
+INSERT INTO `system_config` VALUES (30, 'redis_port', '', 'redis服务端口（默认: 6379）', 27, 'number', NULL, 0);
+INSERT INTO `system_config` VALUES (31, 'redis_password', '', 'redis服务密码', 27, 'password', NULL, 0);
+INSERT INTO `system_config` VALUES (32, 'redis_timeout', '2000', '网站连接redis服务超时时间，单位毫秒', 27, 'number', NULL, 0);
+INSERT INTO `system_config` VALUES (33, 'redis_database', '0', '网站连接redis服务的哪个数据库，默认0号数据库，取值范围0-15', 27, 'number', NULL, 0);
+INSERT INTO `system_config` VALUES (35, NULL, NULL, 'Elasticsearch配置', 0, NULL, NULL, 0);
+INSERT INTO `system_config` VALUES (36, 'elasticsearch_host', '', 'elasticsearch服务的地址', 35, 'text', NULL, 0);
+INSERT INTO `system_config` VALUES (37, 'elasticsearch_port', '', 'elasticsearch服务的http端口', 35, 'number', NULL, 0);
+INSERT INTO `system_config` VALUES (38, 'elasticsearch_index', '', '索引的名字', 35, 'text', NULL, 0);
+INSERT INTO `system_config` VALUES (39, 'search', '0', '是否开启搜索功能（如果开启，需要额外启动一个ES服务，并填好ES相关的配置）', 35, 'radio', NULL, 0);
+INSERT INTO `system_config` VALUES (40, NULL, NULL, 'Github配置', 0, '', NULL, 0);
+INSERT INTO `system_config` VALUES (41, 'oauth_github_client_id', '', 'Github登录配置项ClientId', 40, 'text', NULL, 0);
+INSERT INTO `system_config` VALUES (42, 'oauth_github_client_secret', '', 'Github登录配置项ClientSecret', 40, 'password', NULL, 0);
+INSERT INTO `system_config` VALUES (43, 'oauth_github_callback_url', '', 'Github登录配置项回调地址', 40, 'url', NULL, 0);
+INSERT INTO `system_config` VALUES (48, 'theme', 'default', '系统主题', 23, 'select', 'default', 0);
+INSERT INTO `system_config` VALUES (59, 'sms_tencent_app_id', ' ', '腾讯云短信appId', 61, 'number', NULL, 0);
+INSERT INTO `system_config` VALUES (60, 'sms_tencent_app_key', ' ', '腾讯云短信appKey', 61, 'text', NULL, 0);
+INSERT INTO `system_config` VALUES (61, NULL, NULL, '腾讯云短信配置', 0, NULL, NULL, 0);
+INSERT INTO `system_config` VALUES (62, NULL, '', '企业微信配置', 0, NULL, NULL, 0);
+INSERT INTO `system_config` VALUES (63, 'appId', '', '企业微信appId', 62, 'text', NULL, 0);
+INSERT INTO `system_config` VALUES (64, 'appSecret', '', '企业微信appSecret', 62, 'text', NULL, 0);
+INSERT INTO `system_config` VALUES (65, 'token', '', '企业微信token', 62, 'text', NULL, 0);
+INSERT INTO `system_config` VALUES (66, 'agentId', '', '企业微信应用id（agentId）', 62, 'text', NULL, 0);
 
 /*!40000 ALTER TABLE `system_config` ENABLE KEYS */;
 UNLOCK TABLES;
