db20fa71d6c05cd75b407a8d1939f1f57a6ba4b6
Liugz <liuguozhu30@163.com>
2019-06-25 15:52:43 +0800

    修改网络监测的方法，因为InetAddress.isReachable不靠谱，详见代码注释

--- src/main/java/com/coder/monitor/task/NetworkMonitoring.java
+++ src/main/java/com/coder/monitor/task/NetworkMonitoring.java
@@ -25,53 +25,78 @@ public class NetworkMonitoring {
     public void checkNetwork() {
         logger.debug("check Network begin.");
         String host = "www.baidu.com";
-        boolean isReachable = false;
-        try {
-            InetAddress address = InetAddress.getByName(host);//ping this IP
-//            String hostAddress = address.getHostAddress();
-//            String hostName = address.getHostName();
-            if (address instanceof java.net.Inet4Address) {
-                logger.info(host + " is ipv4 address");
-            } else if (address instanceof java.net.Inet6Address) {
-                logger.info(host + " is ipv6 address");
-            } else {
-                logger.info(host + " is unrecongized");
-            }
-
-            if (address.isReachable(1000)) {
-                isReachable = true;
-                logger.info("SUCCESS - ping " + host + " with no interface specified");
-            } else {
-                logger.info("FAILURE - ping " + host + " with no interface specified");
-            }
-            logger.info("\n-------Trying different interfaces--------\n");
-
-            Enumeration<NetworkInterface> netInterfaces = NetworkInterface.getNetworkInterfaces();
-            while (netInterfaces.hasMoreElements()) {
-                NetworkInterface ni = netInterfaces.nextElement();
-                logger.info("Checking interface, DisplayName:" + ni.getDisplayName() + ", Name:" + ni.getName());
-                if (address.isReachable(ni, 0, 1000)) {
-                    isReachable = true;
-                    logger.info("SUCCESS - ping " + host);
-                } else {
-                    logger.info("FAILURE - ping " + host);
-                }
-
-                Enumeration<InetAddress> ips = ni.getInetAddresses();
-                while (ips.hasMoreElements()) {
-                    logger.info("IP: " + ips.nextElement().getHostAddress());
-                }
-                logger.info("-------------------------------------------");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        if (!isReachable) {
+        if (!isNodeReachable(host)) {
             logger.debug("DB开始发送异常消息");
             sendMessageService.sendMessage("FAILURE - ping " + host);
         }
         logger.debug("check Network end.");
+        return;
+
+//        boolean isReachable = false;
+//        try {
+//            InetAddress address = InetAddress.getByName(host);//ping this IP
+////            String hostAddress = address.getHostAddress();
+////            String hostName = address.getHostName();
+//            if (address instanceof java.net.Inet4Address) {
+//                logger.info(host + " is ipv4 address");
+//            } else if (address instanceof java.net.Inet6Address) {
+//                logger.info(host + " is ipv6 address");
+//            } else {
+//                logger.info(host + " is unrecognized");
+//            }
+//
+//
+//            /*
+//            Test whether that address is reachable. Best effort is made by the implementation to try to reach the host, but firewalls and server configuration may block requests resulting in a unreachable status while some specific ports may be accessible.
+//            A typical implementation will use ICMP ECHO REQUESTs if the privilege can be obtained,
+//            otherwise it will try to establish a TCP connection on port 7 (Echo) of the destination host.
+//            上面英文第二行的意思是，如果本机的权限允许，那么isReachable采用跟PING一样的方式取连接远程服务器。 但是默认情况下，你绝逼没有这个权限。
+//            上面英文第三行的意思是，要是没有权限，那么尝试TCP连接远程服务器的7号端口。所以，如果你能够telnet ip 7成功的话，isReachable返回就是true了。
+//             */
+//            if (address.isReachable(1000)) {
+//                isReachable = true;
+//                logger.info("SUCCESS - ping " + host + " with no interface specified");
+//            } else {
+//                logger.info("FAILURE - ping " + host + " with no interface specified");
+//            }
+//            logger.info("\n-------Trying different interfaces--------\n");
+//
+//            Enumeration<NetworkInterface> netInterfaces = NetworkInterface.getNetworkInterfaces();
+//            while (netInterfaces.hasMoreElements()) {
+//                NetworkInterface ni = netInterfaces.nextElement();
+//                logger.info("Checking interface, DisplayName:" + ni.getDisplayName() + ", Name:" + ni.getName());
+//                if (address.isReachable(ni, 0, 1000)) {
+//                    isReachable = true;
+//                    logger.info("SUCCESS - ping " + host);
+//                } else {
+//                    logger.info("FAILURE - ping " + host);
+//                }
+//
+//                Enumeration<InetAddress> ips = ni.getInetAddresses();
+//                while (ips.hasMoreElements()) {
+//                    logger.info("IP: " + ips.nextElement().getHostAddress());
+//                }
+//                logger.info("-------------------------------------------");
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        if (!isReachable) {
+//            logger.debug("DB开始发送异常消息");
+//            sendMessageService.sendMessage("FAILURE - ping " + host);
+//        }
+//        logger.debug("check Network end.");
     }
+
+    private static boolean isNodeReachable(String hostname) {
+        try {
+            return 0 == Runtime.getRuntime().exec("ping -c 1 " + hostname).waitFor();
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
 
     //    @Scheduled(fixedDelay = 5000)
 //    @Scheduled(cron = "0 0/5 * * * ?")//每5分钟执行一次
